9b9db35e59af3dc595f2828cec71392d6ef511ae
Keahooi Hung <keahung@berkeley.edu>
2018-02-02 18:51:00 -0800

    Added first draft of auto lock feature

--- Code/controller/controller.ino
+++ Code/controller/controller.ino
@@ -3,31 +3,58 @@ const int tiltPin = A1;  // High or low voltage "bool"
 const int pwmPin1 = 5;   // Output to the tilt lock servo
 const int pwmPin2 = 6;   // Output to the brake servo for one wheel
 const int pwmPin3 = 7;   // Output to the brake servo for the second wheel
-             
-int potVal;
-bool tiltVal;
 
-bool locked;
-int tiltOutput;
-int brakeOutput;
-int minServoRange;
-int maxServoRange;
+int potVal;    // value read in from brake lever
+
+bool locked = false;              // True if lever is held in same position for 3 seconds = 3000 ms
+int servoVal;                     // potval converted to servo val
+unsigned long timeAtChange = 0;   // Time at which the brake lever position last changed
+int prevServoVal = 0;
+int lockedVal = 0;
+
+int servoOutput;             // same servo output for brake and tilt lock
+int minServoRange = 0;
+int maxServoRange = 280;
 
 
 void setup() {
   pinMode(pwmPin1, OUTPUT);  
   pinMode(pwmPin2, OUTPUT);
+  pinMode(pwmPin3, OUTPUT);
 }
 
 void loop() {
-  potVal = analogRead(potPin);    // read values from brake & tilt input
-  tiltVal = digitalRead(tiltPin); // read in boolean value 
+  potVal = analogRead(potPin);
+  servoVal = convertToServo(potVal);
 
-  tiltOutput = convertTiltOutput(tiltVal);
-  brakeOutput = convertBrakeOutput(potVal);
+  if (locked == true) {
+    if (servoVal > prevServoVal) {
+      locked = false;
+      servoOutput = servoVal;
+      updateTimeAndVals();
+    }
+    else {
+      servoOutput = lockedVal;
+      updateTimeAndVals();
+    }
+  }
+  
+  else if (locked != true) {
+    if (servoVal != prevServoVal) {
+      servoOutput = servoVal;
+      updateTimeAndVals();
+    } 
+    else {
+      if (millis() - timeAtChange > 3000) {  // dont update timeatchange
+        locked = true;
+        lockedVal = servoVal;
+        servoOutput = servoVal;
+      }
+    }
+  }
 
-  digitalWrite(pwmPin1, tiltOutput);
-  digitalWrite(pwmPin2, brakeOutput);  // outputs the same value for both wheels
-  digitalWrite(pwmPin3, brakeOutput);
+  digitalWrite(pwmPin1, servoOutput);   
+  digitalWrite(pwmPin2, servoOutput);  // outputs the same value for both wheels and tiltlock
+  digitalWrite(pwmPin3, servoOutput);
   delay(10);
 }
--- Code/controller/funcs.ino
+++ Code/controller/funcs.ino
@@ -1,14 +1,12 @@
-int convertTiltOutput(bool value) {
-  if (value) {
-    return maxServoRange;
-  }
-  return minServoRange;
-}
-
-int convertBrakeOutput(int value) {
-  int minValue;
+int convertToServo(int value) {
+  int minValue;                           // remember to set these based on voltage range
   int maxValue;
   int ratio = value / (maxValue - minValue);
   return (ratio * (maxServoRange - minServoRange)) + minServoRange;
+}
+
+void updateTimeAndVals() {
+  prevServoVal = servoVal;
+  timeAtChange = millis();
 }
 

0223bf58486f7c8eb87df98c02aea28a8158c0f5
harrisonhsueh <harrison.hsueh@berkeley.edu>
2018-02-06 11:31:58 -0800

    added constraint to servo angle to protect it

--- Code/controller/funcs.ino
+++ Code/controller/funcs.ino
@@ -1,8 +1,10 @@
 int convertToServo(double value) {
-  double minValue = 600;           // volts
+  double minValue = 600;                                                  // 0-3.3V converted to 0-1024
   double maxValue = 1024;
   double ratio = (value - minValue) / (maxValue - minValue);
-  return (ratio * (maxServoRange - minServoRange)) + minServoRange;
+  int angle = (ratio * (maxServoRange - minServoRange)) + minServoRange;
+  constrain(angle,minServoRange,maxServoRange);                           //protect servo in case voltage input goes outside of range
+  return angle;
 }
 
 void updateTimeAndVals() {

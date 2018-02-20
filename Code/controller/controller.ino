0b061b3d86843aa5a0ad2695d5069f94f2e7a3c1
harrisonhsueh <harrison.hsueh@berkeley.edu>
2018-02-20 13:34:42 -0800

    cleaned up controller code and increased smoothing to 5 samples which corresponds to 50ms

--- Code/controller/controller.ino
+++ Code/controller/controller.ino
@@ -1,5 +1,9 @@
 #include "Servo.h"
 #include "AnalogSmoothInt.h"
+#include "ParkingLock.h"
+
+/*pins*/
+const int batteryPin = A5;    // Battery voltage divided down by 10
 const int brakePin1 = A0;     // Braking input: ranges from min value to max value, should convert to servo value 0-180 deg
 const int brakePin2 = A1;     // Braking input: ranges from min value to max value, should convert to servo value 0-180 deg
 const int tiltPin = A2;       // Tilt lever input: ranges from the same values as potpin
@@ -7,32 +11,37 @@ const int brakePWMPin1 = 5;   // Output to the brake servo for one wheel
 const int brakePWMPin2 = 6;   // Output to the brake servo for the second wheel 
 const int tiltPWMPin = 7;     // Output to the tilt lock servo
 
-float brakeRead1;             // value read in from brake lever
-float tiltRead;               // value read in from tilt lever
+/*battery*/
+const int batteryVoltageCutoff = 3.6*4/3.3*1023/10;  // 
+int batteryVoltage;
+bool batteryGood;
 
-bool locked = false;                  // True if tilt lever is held in same position for 3 seconds = 3000 ms
-unsigned long timeAtChange = 0;       // Time at which the brake lever position last changed
-int minTiltServoOutput = 0;
-int lockedTiltServoOutput = 0;        // Locked tilt lock value
-
-int tiltServoOutput;                  // servo output for tiltlock
-int brakeServoOutput;                 // servo output for brake
-
-int minValue = 2.380 * 1023/3.3;      //1.1V to 1.2V or 2.38V to 2.93V depending on potentiometer position
-int maxValue = 2.930 * 1023/3.3;
-int minHighTime = 800;                //min High time for PWM to servo, according to specs
-int maxHighTime = 2200;               //max High time for PWM to servo, according to specs
-int minServoRange = 100;              //0 degrees would output minHighTime
-int maxServoRange = 175;              //180 degrees would ouput maxHighTime
-
+/*LEVERS AND SERVOS variables*/
+float brakeRead1;                     // value read in from brake lever after being smoothed
+float tiltRead;                 q     // value read in from tilt lever after being smoothed
+const int minValue = 2.380 * 1023/3.3;      //1.1V to 1.2V or 2.38V to 2.93V depending on potentiometer position
+const int maxValue = 2.930 * 1023/3.3;
+const int minHighTime = 800;                //min High time for PWM to servo, according to specs
+const int maxHighTime = 2200;               //max High time for PWM to servo, according to specs
+const int minServoRange = 100;              //0 degrees would output minHighTime
+const int maxServoRange = 175;              //180 degrees would ouput maxHighTime
+int brakeServoOutputProcessed1;             // after processed for locking
+int tiltServoOutputProcessed;               // after processed for locking
+int brakeServoOutput1;                      // servo output for brake
+int tiltServoOutput;                        // servo output for tiltlock
+/*For smoothing break input*/
+AnalogSmoothInt smoothedBrake1 = AnalogSmoothInt(5);
+AnalogSmoothInt smoothedTilt = AnalogSmoothInt(5);
+/*For analyiing input for parking lock*/
+const int periodBrakeRead = 10;             //period of brake input interrupt reading in ms
+boolean leverReadTicked = 0;                //keep track of whether brake was read
+const int lockingTime = 3000;               //ms until levers lock
+const int lockingTol = 9;                   //tolerance for lever values to lock
+ParkingLock brakeLock1 = ParkingLock(1000/periodBrakeRead,lockingTime, lockingTol);
+ParkingLock tiltLock = ParkingLock(1000/periodBrakeRead,lockingTime, lockingTol);
+/*Servo Objects*/
 Servo brakeServo1;
 Servo tiltServo;
-
-/*for smoothing break input*/
-AnalogSmoothInt smoothedBrake1 = AnalogSmoothInt(3);
-
-int periodBrakeRead = 10;             //period of brake input reading in ms
-boolean leverReadTicked = 0;          //keep track of whether brake was read
 
 void setup() {  
   brakeServo1.attach(brakePWMPin1,minHighTime,maxHighTime);
@@ -44,33 +53,26 @@ void setup() {
 }
 
 void loop() {
-  if (!(millis()%periodBrakeRead) && !leverReadTicked) {
+  checkBat(); //checks battery voltage and sets batteryGood
+  
+  if (!(millis()%periodBrakeRead) && !leverReadTicked && batteryGood) {
     /*brake 1*/
     brakeRead1 = smoothedBrake1.analogReadSmooth(brakePin1);
-    brakeServoOutput = convertToServo(brakeRead1);
-    //brakeServo1.write(brakeServoOutput); 
-
+    brakeServoOutput1 = convertToServo(brakeRead1);
+    brakeServoOutputProcessed1 = brakeLock1.processParkingLock(brakeServoOutput1);
+    brakeServo1.write(brakeServoOutputProcessed1); 
     /*tilt*/
-    tiltRead = analogRead(tiltPin);
+    tiltRead = smoothedTile.analogReadSmooth(tiltPin);
     tiltServoOutput = convertToServo(tiltRead);
-    
-    tiltServoOutput = brakeServoOutput;
-    //Serial.println("voltage");
-    Serial.print(brakeRead1/8);
-    Serial.print("\t");
-    Serial.print(tiltServoOutput);
-    Serial.print("\t");
-    
-    processTiltServoOutput();
-    Serial.print(tiltServoOutput);
+    tiltServoOutputProcessed = tiltLock.processParkingLock(tiltServoOutput);
+    tiltServo.write(tiltServoOutputProcessed); 
+
+    leverReadTicked = 1;    
+    Serial.print(brakeServoOutput1); Serial.print("\t");
+    Serial.print(brakeServoOutputProcessed1); Serial.print("\t");
     Serial.println();
-    
-    brakeServo1.write(tiltServoOutput); 
-    tiltServo.write(tiltServoOutput); 
-    
-    leverReadTicked = 1;
   } else if (millis()%periodBrakeRead) {
     leverReadTicked = 0;
   }
-
+  
 }

b70e1b66cafa817a2dc5c2b569d1c83f753d3683
Keahooi Hung <keahung@berkeley.edu>
2018-03-01 22:57:51 -0800

    moved AccelSpeed to libraries

---
+++ Code/libraries/AccelSpeed/AccelSpeed.h
@@ -0,0 +1,28 @@
+/*
+  Accelerometer.h - Library for calculating vehicle speed.
+  Created by Harrison C. Hsueh, Keahooi Hung, November 12 2017.
+  Released into the public domain.
+*/
+#ifndef AccelSpeed_h
+#define AccelSpeed_h
+#include "Arduino.h"
+
+class AccelSpeed {
+  public:
+    AccelSpeed(int freq);
+    void setAccel();         //HH: void since this function can just set the 'acceleration' variable for other functions to use
+    volatile boolean _interrupted;      // set True when readAccel is called, set back to False after updating speed
+    void updateSpeed(int wheelSpeed, boolean braking);  // function updating speed
+    float _vehicleSpeed;       // speed
+    volatile float _aix;
+    volatile float _aiy;
+    volatile float _aiz;
+  
+  private:
+    int _freq;
+    int _brakeTime;    
+};
+
+#endif
+
+
---
+++ Code/libraries/AccelSpeed/AccelSpeed.ino
@@ -0,0 +1,44 @@
+#include "AccelSpeed.h"
+#include "CurieTimerOne.h"
+
+unsigned long start_time = 0;
+unsigned long end_time = 0;
+
+const int timePeriod = 10000;               // frequency that timer will update (1000 microseconds = .001 sec) //Hchaged to 10ms for now
+AccelSpeed myAccelSpeed(100);         // create AccelSpeed object with some frequency //change to 100Hz
+
+int resetPeriod = 10;     // reset period in seconds
+int resetDuration = 1;    // reset duration in seconds
+boolean braking = true;
+
+void timedUpdateSpeedISR() {
+  myAccelSpeed.setAccel();
+}
+
+void setup() {
+  
+  CurieTimerOne.start(timePeriod, &timedUpdateSpeedISR);  // will run timedUpdateSpeed interrupt every .01 seconds
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Serial.print("velocity: ");
+  Serial.print(myAccelSpeed._vehicleSpeed);
+  Serial.print("\t");
+  //Serial.print("accel: ");
+  Serial.println(myAccelSpeed._aix);
+  
+  if (myAccelSpeed._interrupted) {
+    myAccelSpeed.updateSpeed(0, braking);    // placeholders for wheelspeed and braking
+  }
+    
+  
+  if ((millis()/(1000*resetDuration))%(resetPeriod/resetDuration)==0) {
+    braking = false;
+  }
+  else {
+    braking = true;
+  }
+}
+
+

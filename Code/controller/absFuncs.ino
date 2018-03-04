d3e49c4a707a024de6001dbb6ae52607164d4ca0
Harrison Hsueh <harrison.hsueh@berkeley.edu>
2018-03-03 16:13:04 -0800

    controlerl codee all mphX100

--- Code/controller/absFuncs.ino
+++ Code/controller/absFuncs.ino
@@ -1,7 +1,7 @@
 /*called by brake which is 10Hz*/
 void antiLockBrake() {
-  //antiLockBrake(myAccelSpeed._vehicleSpeedX10, wheel2._mphX10, 100, 100);
-  absOutputWheel1 = absWheel1.processABS(myAccelSpeed._vehicleSpeed*22.4, wheel2._mphX10, brakeServoOutputProcessed1);
-  absOutputWheel2 = absWheel2.processABS(myAccelSpeed._vehicleSpeed*22.4, wheel2._mphX10, brakeServoOutput2);
+  //antiLockBrake(myAccelSpeed._vehicleSpeedX10, wheel2._mphX100, 100, 100);
+  absOutputWheel1 = absWheel1.processABS(myAccelSpeed._mphX100, wheel2._mphX100, brakeServoOutputProcessed1);
+  absOutputWheel2 = absWheel2.processABS(myAccelSpeed._mphX100, wheel2._mphX100, brakeServoOutput2);
 }
 
